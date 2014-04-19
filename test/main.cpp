04ce8906cb475cb541dd29d211934fe22cc855df
Andy Stanton <andystanton@gmail.com>
2014-04-19 18:04:20 +0100

    base cmake project

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * This file includes the magic necessary in order to get your unit tests
+ * that you create with UnitTest++ to automatically run. There should
+ * never be a reason to modify this file. For an example unit test,
+ * see "sanity_check.cpp". For a reference on creating tests, see "test.h".
+ *
+ */
+#include <iostream>
+
+using namespace::std;
+
+int main(int argc, char* argv[])
+{
+    cout << "test hello world\n";
+}
