821b4509ef14498a1788db60ce8735c142024d4a
Andy Stanton <andystanton@gmail.com>
2014-04-20 00:52:59 +0100

    working with bandit

--- test/main.cpp
+++ test/main.cpp
@@ -1,18 +1,18 @@
-/*
- * This file includes the magic necessary in order to get your unit tests
- * that you create with UnitTest++ to automatically run. There should
- * never be a reason to modify this file. For an example unit test,
- * see "sanity_check.cpp". For a reference on creating tests, see "test.h".
- *
- */
-
- #include <gtest/gtest.h>
+#include <bandit/bandit.h>
+using namespace bandit;
 
 int adder(int a, int b) {
     return a + b;
 }
 
-TEST(AdderTest, Basic) {
-  EXPECT_EQ(3, adder(1, 2));
-  EXPECT_EQ(50, adder(26, 24));
+go_bandit([](){
+    describe("an adder:", [](){
+      it("adds two numbers together", [&](){
+        AssertThat(adder(4, 7), Equals(11));
+      });
+    });
+});
+
+int main(int argc, char* argv[]) {
+  return bandit::run(argc, argv);
 }
