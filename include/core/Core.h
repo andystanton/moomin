696143d67c570910ad8c0b527b5a66298193a1b4
Andy Stanton <andystanton@gmail.com>
2014-05-03 20:07:29 +0100

    clear out for github pages

--- include/core/Core.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MOOMIN_CORE
-#define _MOOMIN_CORE
-
-#define M_PI 3.1415927
-
-#endif
--- test/banditrunner.cpp
+++
@@ -1,6 +0,0 @@
-#include "BanditWithGmock.h"
-
-int main(int argc, char* argv[])
-{
-    return bandit_with_gmock::run(argc, argv);
-}
