12a4361564c5b65cf237d518cba94785211fc3f3
Andy Stanton <andystanton@gmail.com>
2014-05-28 08:34:43 +0100

    #69 fix lessThanOrAlmostEqual and moreThanOrAlmostEqual

--- src/util/FloatingPointHelper.cpp
+++ src/util/FloatingPointHelper.cpp
@@ -9,12 +9,12 @@ bool floatingPointHelper::almostEqual(float x, float y, int ulp)
 
 bool floatingPointHelper::lessThanOrAlmostEqual(float x, float y, int ulp)
 {
-    return almostEqual(x, y, ulp) ^ (x < y);
+    return almostEqual(x, y, ulp) || (x < y);
 }
 
 bool floatingPointHelper::moreThanOrAlmostEqual(float x, float y, int ulp)
 {
-    return almostEqual(x, y, ulp) ^ (x > y);
+    return almostEqual(x, y, ulp) || (x > y);
 }
 
 bool floatingPointHelper::significantlyLessThan(float x, float y, int ulp)
--- test/spec/util/FloatingPointHelper.spec.cpp
+++ test/spec/util/FloatingPointHelper.spec.cpp
@@ -16,5 +16,28 @@ go_bandit([]()
             AssertThat((val1 == val2), Is().False());
             AssertThat(floatingPointHelper::almostEqual(val1, val2), Is().True());
         });
+
+        it("compares floats", [&]()
+        {
+            float val1 = 5;
+            float val2 = 5.000001;
+
+            float control = 3432;
+
+            AssertThat(floatingPointHelper::significantlyLessThan(val1, val2), Is().False());
+            AssertThat(floatingPointHelper::significantlyLessThan(val2, val1), Is().False());
+
+            AssertThat(floatingPointHelper::lessThanOrAlmostEqual(val1, val2), Is().True());
+            AssertThat(floatingPointHelper::lessThanOrAlmostEqual(val2, val1), Is().True());
+
+            AssertThat(floatingPointHelper::significantlyLessThan(val1, control), Is().True());
+            AssertThat(floatingPointHelper::significantlyLessThan(val2, control), Is().True());
+
+            AssertThat(floatingPointHelper::significantlyMoreThan(val1, val2), Is().False());
+            AssertThat(floatingPointHelper::significantlyMoreThan(val2, val1), Is().False());
+
+            AssertThat(floatingPointHelper::significantlyMoreThan(control, val1), Is().True());
+            AssertThat(floatingPointHelper::significantlyMoreThan(control, val2), Is().True());
+        });
     });
 });
